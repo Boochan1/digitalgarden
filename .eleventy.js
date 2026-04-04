d4baac650a966dcba0f86b439aca482d40493588
gabbyeps <47514463+Boochan1@users.noreply.github.com>
2026-04-04 22:58:06 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -694,6 +694,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
   eleventyConfig.addPassthroughCopy({ "src/site/tracker.html": "/tracker.html" });
+  eleventyConfig.addPassthroughCopy("src/site/css");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
